bd0a4b5eb366dc3f23603180fa81952645d52d84
grzegorzeryks <grzegorzeryks@gmail.com>
2018-05-15 13:28:06 +0200

    footer js manipulation

--- js/main.js
+++ js/main.js
@@ -3,19 +3,29 @@
 
 let menuUl = document.querySelector('.header-menu');
 let logo = document.querySelector('.logo');
-console.log(menuUl);
-var allLi = menuUl.querySelectorAll("li");
-console.log(allLi);
+let allLi = menuUl.querySelectorAll('li');
+let footerArrow = document.querySelector('.footer-arrow');
+let footer = document.querySelector('footer');
 
-for (var i = 0; i < allLi.length; i++) {
+for (let i = 0; i < allLi.length; i++) {
   allLi[i].addEventListener('mouseover', peperShow);
   allLi[i].addEventListener('mouseout', peperOut);
 }
 
+footerArrow.addEventListener('click', footerShow);
+
+function footerShow() {
+  if (footer.style.bottom != '0px') {
+    footer.style.bottom = '0px';
+  } else {
+    footer.style.bottom = '-68px';
+  }
+}
+
 function peperShow() {
-  console.log('on');
   logo.classList.add('logo-move');
 }
+
 function peperOut() {
   logo.classList.remove('logo-move');
 }
